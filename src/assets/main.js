3a88607eeaec83f85ecba4eea2f55ce8efe30201
pifmat <pif.matteo@gmail.com>
2017-05-02 12:33:37 +0200

    modify
    test

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,31 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if(answer == '' || attempt = ''){
+      setHiddenFields();
+    }
 }
 
 //implement new functions here
+function setHiddenFields(){
+  answer = Math.floor(Math.random());
+  answer.toString();
+  while(answer.length < 4){
+    answer = '0' + answer;
+  }
+  attempt = 0;
+}
+function setMessage(label){
+   document.getElementById('message').innerHTML()
+}
+
+
+function validateInput(input){
+  if(input.length == 4){
+    return true;
+    console.log();
+  }else{
+    setMessage("Guesses must be exactly 4 characters long.");
+    return false;
+  }
+}
